a1eab041f035b047478e21742f30a7cc2b74fc8b
Utshant Gurung <utshantgurung@gmail.com>
2021-06-01 13:00:29 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,16 +2,9 @@ def my_collect(array)
   i = 0
   collection = []
   while i < array.size
-    array.collect do |array|
-      collection << lang.upcase
+      collection << yield(array[i])
       i += 1
     end
   end
   collection
-  yield(array)
 end
-
-array.collect {|name| name.split(" ").first}
-
-
-
