369414ddc9b072ca1fd2a8a284fafbe03f2f902d
Utshant Gurung <utshantgurung@gmail.com>
2021-06-01 12:29:39 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,13 +2,15 @@ def my_collect(array)
   i = 0
   collection = []
   while i < array.size
-    my_collect(array) do |lang|
+    array.collect do |lang|
       collection << lang.upcase
       i += 1
     end
   end
   collection
 end
+
+
 
 
 
